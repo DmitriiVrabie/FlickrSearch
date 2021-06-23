41c7f8cdc24e0ad3bf84e552f2efb1d215a9a710
dmvrabie <dmitrii.vrabie@endava.com>
2021-06-23 21:39:44 +0300

    Added SearchView

--- app/src/main/java/com/dvrabie/flickrsearch/SearchActivity.kt
+++ app/src/main/java/com/dvrabie/flickrsearch/SearchActivity.kt
@@ -1,11 +1,31 @@
 package com.dvrabie.flickrsearch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        val searchItem: MenuItem? = menu.findItem(R.id.search)
+        val searchView: SearchView = searchItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
